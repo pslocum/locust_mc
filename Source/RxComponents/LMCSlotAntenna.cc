cd80be14936611af5ef998a6f30b7ca171dd76af
Penny Slocum <penny.slocum@yale.edu>
2020-05-06 15:23:10 -0400

    Removed 4 degree directivity cut in LMCSlotAntenna.  Applied Gaussian bandpass filter around carrier with width 25 MHz in LMCArraySignalGenerator.

--- Source/RxComponents/LMCSlotAntenna.cc
+++ Source/RxComponents/LMCSlotAntenna.cc
@@ -44,11 +44,8 @@ namespace locust
     	// dipole donut pinch from HFSS, normalized to 1.0 for normal incidence.
     	double dipolePhiPinchFactor = cos(tPhi);
 
-    	if (fabs(tTheta-LMCConst::Pi()/2.)>4.*LMCConst::Pi()/180.)  // 4 degree aoi cut.
-    	{
-    		return dipoleThetaFactor * dipolePhiPinchFactor;
-    	}
-    	else return 0.;
+    	return dipoleThetaFactor * dipolePhiPinchFactor;
+
     }
 
 
