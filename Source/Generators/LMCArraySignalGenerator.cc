f13f04eb1fad3239e6d0888de0f675f61d2792a8
Penny Slocum <penny.slocum@yale.edu>
2021-01-18 17:00:25 -0500

    Wait 400 ms longer for Kassiopeia to trigger event.  This is a workaround that temporarily replaces development of new shared variables on the Kassiopeia side.  It addresses the problem of Locust occasionally finishing before Kassiopeia triggers an event.

--- Source/Generators/LMCArraySignalGenerator.cc
+++ Source/Generators/LMCArraySignalGenerator.cc
@@ -622,7 +622,7 @@ namespace locust
                         fInterface->fEventInProgress = true;
                         LPROG( lmclog, "LMC about to WakeBeforeEvent()" );
                         WakeBeforeEvent();  // trigger Kass event.
-                        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                        std::this_thread::sleep_for(std::chrono::milliseconds(500));
                     }
                 }
 
